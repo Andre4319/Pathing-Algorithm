9d765b572903b500d5c20940b5b7df955d385ce1
André Eriksson <andre.eriksson.4319@gmail.com>
2023-02-14 22:26:17 +0100

    Added functionality for different dimensions
    You can now have custom dimensions such as 15x3 or 10x100 instead of the same x and y (9x9)! Also added the z index to start and end positions for 3d maps. May need to be simplified as to reduce redundancy.

--- maps.ts
+++ maps.ts
@@ -2,14 +2,17 @@ let fs = require('fs');
 import * as PNGSync from "pngjs/lib/png-sync";
 
 type Position = [ x: number, y: number ]
-type FixedPositions = { startPos: Position, endPos: Position }
+type Position3D = [x: number, y: number, z: number ]
+type FixedPositions2D = { startPos: Position, endPos: Position }
+type FixedPositions3D = { startPos: Position3D, endPos: Position3D }
 
-type Image2D = { path: string }
-type Image3D = { path: string, area: number }
+type Dimension = {box: FixedPositions2D, width: number, height: number}
+type Image2D = { path: string, dimensions: Dimension }
+type Image3D = { path: string, dimensions: Dimension }
 
 type MapArray = { obstacles: Position[] | undefined, traversable: Position[] }
-type Map2D = { fixedPositions: FixedPositions | undefined, map: MapArray }
-type Map3D = { fixedPositions: FixedPositions | undefined, zIndex: Map<number, MapArray> }
+type Map2D = { fixedPositions: FixedPositions2D | undefined, map: MapArray }
+type Map3D = { fixedPositions: FixedPositions3D | undefined, zIndex: Map<number, MapArray> }
 
 
 export function position(x: number, y: number): Position {
@@ -34,6 +37,13 @@ export function get3DMap(img: Image3D): Map3D {
     return load3D(img);
 }
 
+function getMapDimensions(boundingBox: FixedPositions2D): Dimension {
+    const height = boundingBox.endPos[1] - boundingBox.startPos[1];
+    const width = boundingBox.endPos[0] - boundingBox.startPos[0];
+
+    return {box: boundingBox, width: width, height: height}
+}
+
 function InvalidMapException(message: string) {
     this.message = message;
     this.name = "InvalidMapException";
@@ -44,12 +54,17 @@ function InvalidMapException(message: string) {
  * @param img The image to be loaded
  * @returns A 2D Map
  */
-function load(img: Image2D, area: number, width: number, startPos: Position): Map2D {
+function load(img: Image2D): Map2D {
     const map_2D: Map2D = {fixedPositions: {startPos: undefined, endPos: undefined}, map: {obstacles: [], traversable: []}}
     const buffer = fs.readFileSync(img.path);
     const png = PNGSync.read(buffer);
-    for (let y = startPos[1]; y < startPos[1] + area; y++) {
-        for (let x = startPos[0]; x < startPos[0] + area; x++) {
+    const { width } = png;
+
+    const baseY = img.dimensions.box.startPos[1];
+    const baseX = img.dimensions.box.startPos[0];
+
+    for (let y = baseY; y < baseY + img.dimensions.height; y++) {
+        for (let x = baseX; x < baseX + img.dimensions.width; x++) {
             const idx = (width * y + x) << 2;
             
             // RGB VALUES
@@ -80,9 +95,8 @@ function load(img: Image2D, area: number, width: number, startPos: Position): Ma
 function load2D(img: Image2D): Map2D {
     const buffer = fs.readFileSync(img.path);
     const png = PNGSync.read(buffer);
-    const { width } = png;
-    //TODO: Add functionality for non-cubed dimensions such as 15x4
-    const map_2D = load({path: img.path}, width, width, position(0, 0))
+    const { width, height } = png;
+    const map_2D = load({path: img.path, dimensions: getMapDimensions({startPos: position(0,0), endPos: position(width, height)})})
 
     if(map_2D.fixedPositions === undefined || map_2D.fixedPositions.startPos === undefined || map_2D.fixedPositions.endPos === undefined) {
         throw new InvalidMapException("Can't obtain a start and/or 'end' position");
@@ -98,24 +112,25 @@ function load2D(img: Image2D): Map2D {
  */
 function load3D(img: Image3D): Map3D {
     let zIndex = 0;
-    let fixed_positions: FixedPositions = {startPos: undefined, endPos: undefined};
+    let fixed_positions: FixedPositions3D = {startPos: undefined, endPos: undefined};
     const allMaps = new Map();
     const { width, height } = PNGSync.read(fs.readFileSync(img.path));
-    const depth = [Math.floor(height / img.area), Math.floor(width / img.area)];
+    const depth = [Math.floor(height / img.dimensions.height), Math.floor(width / img.dimensions.width)];
 
     for (let y = 0; y < depth[0]; y++) {
         for (let x = 0; x < depth[1]; x++) {
-            const zHeight = img.area * y + (y !== 0 ? 1 : 0);
-            const zWidth = img.area * x + (x !== 0 ? 1 : 0);
-            //fixedPositions: {startPos: [zWidth, zHeight], endPos: [zWidth + img.area, zHeight + img.area]}
-            const map_2D = load({path: img.path}, img.area, width, position(zWidth, zHeight));
+            
+            const zHeight = img.dimensions.height * y + (y !== 0 ? 1 : 0);
+            const zWidth = img.dimensions.width * x + (x !== 0 ? 1 : 0);
+            const t = getMapDimensions({startPos: [zWidth, zHeight], endPos: [zWidth + img.dimensions.width, zHeight + img.dimensions.height]});
+            const map_2D = load({path: img.path, dimensions: t})
             const map: MapArray = {obstacles: map_2D.map.obstacles, traversable: map_2D.map.traversable}
             if(map_2D.fixedPositions.startPos != undefined) {
-                    fixed_positions.startPos = map_2D.fixedPositions.startPos;
+                fixed_positions.startPos = [map_2D.fixedPositions.startPos[0], map_2D.fixedPositions.startPos[1], zIndex];
             }
 
             if(map_2D.fixedPositions.endPos != undefined) {
-                    fixed_positions.endPos = map_2D.fixedPositions.endPos;
+                fixed_positions.endPos = [map_2D.fixedPositions.endPos[0], map_2D.fixedPositions.endPos[1], zIndex];
             }
 
             allMaps.set(zIndex, map);
@@ -131,10 +146,13 @@ function load3D(img: Image3D): Map3D {
 }
 
 // 2D Test
-//console.log(load2D({path: './resources/test.png'}))
+//console.log(load2D({path: './resources/test.png', dimensions: getMapDimensions({startPos:[0,0], endPos: [9,9]})}))
 
-// 3D Test #1
-//console.log(load3D({path: './resources/3d_test.png', area: 9}))
+// 3D Test #1 ( Multiple start and end point )
+//console.log(load3D({path: './resources/3d_test.png', dimensions: getMapDimensions({startPos: [0,0], endPos: [9,9]})}))
 
-// 3D Test #2
-//console.log(load3D({path: './resources/3d_test2.png', area: 9}))
+// 3D Test #2 ( One start and end point )
+//console.log(load3D({path: './resources/3d_test2.png', dimensions: getMapDimensions({startPos: [0,0], endPos: [9,9]})}))
+
+// 3D Test #3 ( Non-cubed dimensions & Different zIndex for both start and end points )
+//console.log(load3D({path: './resources/3d_test3.png', dimensions: getMapDimensions({startPos: [0,0], endPos: [9,18]})}))
