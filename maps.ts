ea1f6edca73d63bc4fadd3e524e60e9bacc2dadb
André Eriksson <andre.eriksson.4319@gmail.com>
2023-02-25 17:23:30 +0100

    Full 3D Support in maps.ts
    Full support for 3D maps added. Maybe
    later it should be made in to a class.

--- maps.ts
+++ maps.ts
@@ -51,8 +51,10 @@ interface MapArray {
  * A correct traversable map with nodes
  */
 interface TraversableMap {
+    image: Image;
     fixedNodes: FixedNodes;
     mapDimensions: Dimension;
+    grid?: Grid;
     traversableMaps: MapArray[];
 }
 
@@ -78,7 +80,7 @@ class InvalidMapException extends Error {}
  */
 const ColorDefenitions: Map<string, Color> = new Map<string, Color>([
     ['map_background', {red: 255, green: 255, blue: 255, alpha: 255}],
-    ['map_border',     {red: 0,   green: 0,   blue: 0,   alpha: 255}],
+    ['map_border',     {red: 23,  green: 23,  blue: 23,  alpha: 255}],
     ['map_obstacles',  {red: 0,   green: 0,   blue: 0,   alpha: 255}],
     ['map_origin',     {red: 255, green: 0,   blue: 0,   alpha: 255}],
     ['map_end',        {red: 0,   green: 255, blue: 0,   alpha: 255}],
@@ -105,32 +107,38 @@ export function getNode(x: number, y: number, z: number = 0): Node {
  * @returns A traverasble map
  */
 export function loadImage(image: Image, mapWidth: number, mapHeight: number): TraversableMap {
-    const buffer = fs.readFileSync(`${image.path ?? './resources'}/${image.fileName}`);
+    const fullImage: Image = {fileName: image.fileName, path: image.path ?? './resources'}
+    const buffer = fs.readFileSync(`${fullImage.path}/${fullImage.fileName}`);
     const png = PNGSync.read(buffer);
     const { width, height } = png;
 
-    return load(png, {width, height}, {width: mapWidth, height: mapHeight});
+    return load(image, png, {width, height}, {width: mapWidth, height: mapHeight});
 }
 
+drawPath({fileName: 'test.png'}, {width: 9, height: 9}, [getNode(1, 6), getNode(1, 5), getNode(1, 4), getNode(1, 3), getNode(1, 2), getNode(1, 1), getNode(2, 1), getNode(3, 1), getNode(4, 1), getNode(5, 1), getNode(6, 1)], 
+                                                        [getNode(2, 6), getNode(2, 7), getNode(2, 8)]);
+drawPath({fileName: 'new.png'}, {width: 9, height: 9}, [getNode(4, 4, 3), getNode(4, 3, 3), getNode(4, 2, 3), getNode(4, 1, 3), getNode(4, 1, 1), getNode(3, 1, 1), getNode(2, 1, 1)], 
+                                                       [getNode(2, 6), getNode(2, 7), getNode(2, 8)])
 /**
  * Global function to draw a path with the specified parameters
  * @param image The image to be drawn upon
  * @param correctPath The quickest path from the origin to the end point specified by an algorithm 
  * @param searched What has been searched that isnt the correct path
  */
-export function drawPath(image: Image, correctPath: Array<Node>, searched: Array<Node>) {
-    const targetDirectory = image.path + '/' + image.fileName.split('.')[0] + '/';
+function drawPath(image: Image, mapDimensions: Dimension, correctPath: Array<Node>, searched: Array<Node>) {
+    const path = image.path ?? './resources';
+    const targetDirectory = path + '/' + image.fileName.split('.')[0] + '/';
     fs.mkdirSync(targetDirectory, { recursive: true });
 
-    fs.createReadStream(image.path + '/' + image.fileName)
+    fs.createReadStream(path + '/' + image.fileName)
         .pipe(new PNG.PNG({filterType: 4}))
         .on('parsed', function() {
             searched.forEach(node => {
-                fillNodebyColor(this, node, ColorDefenitions.get('path_searched'));
+                fillNodebyColor(this, mapDimensions, getGlobalNode(node, mapDimensions, getGrid({width: this.width, height: this.height}, mapDimensions)), ColorDefenitions.get('path_searched'), true)
             })
 
             correctPath.forEach(node => {
-                fillNodebyColor(this, node, ColorDefenitions.get('path_correct')); 
+                fillNodebyColor(this, mapDimensions, getGlobalNode(node, mapDimensions, getGrid({width: this.width, height: this.height}, mapDimensions)), ColorDefenitions.get('path_correct'), true) 
             });
             
             // Write the modified image to a file
@@ -146,42 +154,70 @@ export function drawPath(image: Image, correctPath: Array<Node>, searched: Array
  * @param obstacles Nodes which are obstacles
  * @returns A new TraversableMap
  */
-export function createTraversableMap(imageDimension: Dimension, mapDimensions: Dimension, fixedNodes: FixedNodes, obstacles?: Node[]): TraversableMap {
+export function createTraversableMap(image: Image, imageDimension: Dimension, mapDimensions: Dimension, fixedNodes: FixedNodes, obstacles?: Node[]): TraversableMap {
     const traversableMaps: MapArray[] = [];
+    let yDepth = 0;
+    const grid: Grid = getGrid(imageDimension, mapDimensions);
     
-    const grids: Grid = {
-        columns: Math.floor(imageDimension.width / mapDimensions.width), 
-        rows: Math.floor(imageDimension.height / mapDimensions.height),
-    };
+    const png = new PNG.PNG({width: imageDimension.width + grid.columns - 1, height: imageDimension.height + grid.rows - 1});
+    for (let y = 0; y < png.height; y++) {
+        let xDepth = 0;
+        const yBorder = mapDimensions.height * (yDepth + 1) + yDepth;
 
-    for (let z = 0; z < grids.columns + grids.rows; z++) {
-        const traversable: Node[] = [];
-        const allObstacles: Node[] = [];
-        for (let y = 0; y < mapDimensions.height; y++) {
-            for (let x = 0; x < mapDimensions.width; x++) {
-                const node: Node = { x, y, z, };
-                
-                if(obstacles === undefined || compare(obstacles, [])) {
-                    traversable.push(node);
-                } else {
+        yDepth += y > yBorder ? 1 : 0;
 
-                    const isObstacle = obstacles.some(obstacle => {
-                        return obstacle.x === node.x && obstacle.y === node.y && obstacle.z === node.z;
-                    })
+        for (let x = 0; x < png.width; x++) {
+            const globalNode: Node = { x, y, }
 
-                    if(isObstacle) {
-                        allObstacles.push(node);
-                    } else {
-                        traversable.push(node);
-                    }
-                }
+            // FILL IN BACKGROUND
+            fillNodebyColor(png, mapDimensions, globalNode, ColorDefenitions.get('map_background'), false)
+
+            const xBorder = mapDimensions.width * (xDepth + 1) + xDepth
+
+            if(x === xBorder) {
+                fillNodebyColor(png, mapDimensions, globalNode, ColorDefenitions.get('map_border'), false)
+                xDepth++;
+                continue;
             }
+
+            if(y === yBorder) {
+                fillNodebyColor(png, mapDimensions, globalNode, ColorDefenitions.get('map_border'), false)
+                continue;
+            }
+
+            const origin = getGlobalNode(fixedNodes.origin, mapDimensions, grid);
+            const end = getGlobalNode(fixedNodes.end, mapDimensions, grid);
+
+            if(compare(globalNode, {x: origin.x + xDepth, y: origin.y + yDepth})) {
+                fillNodebyColor(png, mapDimensions, globalNode, ColorDefenitions.get('map_origin'), false)
+                continue;
+            } 
+
+            if(compare(globalNode, {x: end.x + xDepth, y: end.y + yDepth})) {
+                fillNodebyColor(png, mapDimensions, globalNode, ColorDefenitions.get('map_end'), false)
+                continue;
+            }
+
+            if(obstacles === undefined || compare(obstacles, [])) { continue; }
+
+            const isObstacle = obstacles.some(obstacle => { const obst = getGlobalNode(obstacle, mapDimensions, grid); return compare(globalNode, {x: obst.x + xDepth, y: obst.y + yDepth})});
+
+            if(isObstacle) {
+                fillNodebyColor(png, mapDimensions, globalNode, ColorDefenitions.get('map_obstacles'), false)
+            }     
         }
-        traversableMaps.push({traversable, obstacles: allObstacles})
+    }
+    const fullImage: Image = {fileName: image.fileName, path: image.path ?? './resources'};
+    const outStream = fs.createWriteStream(`${fullImage.path}/${fullImage.fileName}`);
+    png.pack().pipe(outStream);
+
+    // GET ALL OBSTACLES
+    for (let z = 0; z < grid.columns + grid.rows; z++) { 
+        const nodeData = getAllNodes(png, z, imageDimension, mapDimensions, grid)
+        traversableMaps.push({traversable: nodeData.allNodes.traversable, obstacles: nodeData.allNodes.obstacles})
     }
 
-
-    return { fixedNodes, mapDimensions, traversableMaps };
+    return { image: fullImage, fixedNodes, mapDimensions, traversableMaps };
 }
 
 /**
@@ -246,24 +282,70 @@ function compare<T>(first: T, second: T): boolean {
 }
 
 /**
- * Given a node with depth (z) / independent position you can extract the global position in a 2d grid
- * @param node Where is the node located
+ * Gets the grid size of the image
+ * @param imageDimension The dimension of the image
  * @param mapDimensions The dimensions of each map
- * @param grids How many grids are there
+ * @returns The columns and rows of the image
+ */
+function getGrid(imageDimension: Dimension, mapDimensions: Dimension): Grid {
+    return {
+        columns: Math.floor(imageDimension.width / mapDimensions.width), 
+        rows: Math.floor(imageDimension.height / mapDimensions.height),
+    };
+}
+
+/**
+ * Given a node with depth (z) / independent position you can extract the global position in a 2d grid
+ * @param relativeNode Where is the node located
+ * @param mapDimensions The dimensions of each map
+ * @param grid How many grids are there
  * @returns The global node
  */
-function getGridPosition(node: Node, mapDimensions: Dimension, grids: Grid): Node {
-    const { columns, rows } = grids;
+function getGlobalNode(relativeNode: Node, mapDimensions: Dimension, grid: Grid): Node {
+    const { columns, rows } = grid;
 
     let newY = 0;
-    let newX = (node.z ?? 0) % grids.columns;
+    let newX = (relativeNode.z ?? 0) % grid.columns;
     
-    if(node.z && node.z >= columns && node.z <= columns * rows) {
-        newY = Math.floor(node.z / columns);
+    if(relativeNode.z && relativeNode.z >= columns && relativeNode.z <= columns * rows) {
+        newY = Math.floor(relativeNode.z / columns);
     }
     
-    return { x: node.x + mapDimensions.width * newX, 
-             y: node.y + mapDimensions.height * newY, };
+    return { x: relativeNode.x + mapDimensions.width * newX, 
+             y: relativeNode.y + mapDimensions.height * newY, };
+}
+
+/**
+ * Given a global node without depth. You can extract the relative position inside each grid
+ * @param globalNode The X and Y coordinate in the image
+ * @param mapDimensions The dimensions of each map
+ * @returns The X, Y and Z positions
+ */
+function getRelativeNode(globalNode: Node, mapDimensions: Dimension): Node {
+    const relativeX = globalNode.x % mapDimensions.width;
+    const relativeY = globalNode.y % mapDimensions.height;
+    let relativeZ = 0;
+    let columnDepth = 0;
+
+    if(globalNode.y > mapDimensions.height) {
+        for (let y = globalNode.y - relativeY; y > 0 ; y -= mapDimensions.height) {
+            for (let x = globalNode.x - relativeX; x > 0 ; x -= mapDimensions.width) {
+                relativeZ++;
+            }
+            columnDepth++;
+            relativeZ++;
+        }
+    
+        for(let column = 0; column < columnDepth; column++) {
+            relativeZ++;
+        }
+    } else {
+        for (let x = globalNode.x - relativeX; x > 0 ; x -= mapDimensions.width) {
+            relativeZ++;
+        }
+    }
+    
+    return {x: relativeX, y: relativeY, z: relativeZ}
 }
 
 /**
@@ -274,8 +356,9 @@ function getGridPosition(node: Node, mapDimensions: Dimension, grids: Grid): Nod
  * @param green color between 0 - 255
  * @param blue color between 0 -255
  */
-function fillNode(png:any, node: Node, red: number, green: number, blue: number, alpha: number) {
-    const idx: number = (png.width * node[1] + node[0]) << 2;
+function fillNode(png:any, mapDimensions: Dimension, node: Node, red: number, green: number, blue: number, alpha: number) {
+    const globalNode: Node = getGlobalNode(node, mapDimensions, getGrid({width: png.width, height: png.height}, mapDimensions))
+    const idx: number = (png.width * globalNode.y + globalNode.x) << 2;
     png.data[idx] = limit(red, 0, 255);
     png.data[idx + 1] = limit(green, 0, 255);
     png.data[idx + 2] = limit(blue, 0, 255);
@@ -284,12 +367,25 @@ function fillNode(png:any, node: Node, red: number, green: number, blue: number,
 
 /**
  * Colors in a node with a given position
- * @param png The image
- * @param node The node to color in
+ * @param png The png instance
+ * @param globalNode The node to color in
  * @param color The color values
+ * @param adjustForBorder If the nodes positions should be adjusted in accordance with map borders
  */
-function fillNodebyColor(png:any, node: Node, color: Color) {
-    fillNode(png, node, color.red, color.blue, color.green, color.alpha);
+function fillNodebyColor(png:any, mapDimensions: Dimension, globalNode: Node, color: Color, adjustForBorder: boolean) {
+    if(adjustForBorder) {
+        let yDepth = 0;
+        let xDepth = 0;
+        for (let y = 0; y < globalNode.y; y += mapDimensions.height) {
+            yDepth++;
+        }
+
+        for (let x = 0; x < globalNode.x; x += mapDimensions.width) {
+            xDepth++;
+        }
+        return fillNode(png, mapDimensions, { x: globalNode.x + xDepth - 1, y: globalNode.y + yDepth - 1 }, color.red, color.green, color.blue, color.alpha);
+    }
+    return fillNode(png, mapDimensions, globalNode, color.red, color.green, color.blue, color.alpha);
 }
 
 /**
@@ -324,7 +420,7 @@ function getAllNodes(png: any, z: number, imageDimension: Dimension, mapDimensio
         allNodes:   { traversable: [], obstacles: [] },
     };
 
-    let min: Node = getGridPosition({x: 0, y: 0, z}, mapDimensions, grids);
+    let min: Node = getGlobalNode({x: 0, y: 0, z}, mapDimensions, grids);
 
     for (let y = min.y; y < min.y + mapDimensions.height; y++) {
         for (let x = min.x; x < min.x + mapDimensions.width; x++) {
@@ -339,8 +435,9 @@ function getAllNodes(png: any, z: number, imageDimension: Dimension, mapDimensio
             };
 
             const originColor: Color = ColorDefenitions.get('map_origin');
-            const endColor: Color = ColorDefenitions.get('map_end');
-            const mapColor: Color = ColorDefenitions.get('map_background');
+            const endColor: Color    = ColorDefenitions.get('map_end');
+            const mapColor: Color    = ColorDefenitions.get('map_background');
+            const mapObstacle: Color = ColorDefenitions.get('map_obstacles');
 
             if(compare(mapColor, pixelColor)) {
                 positionData.allNodes.traversable.push(getNode(x,y,z));
@@ -348,7 +445,7 @@ function getAllNodes(png: any, z: number, imageDimension: Dimension, mapDimensio
                 positionData.fixedNodes.origin = getNode(x,y,z);
             } else if(compare(endColor, pixelColor)) {
                 positionData.fixedNodes.end = getNode(x,y,z);
-            } else {
+            } else if(compare(mapObstacle, pixelColor)) {
                 positionData.allNodes.obstacles.push(getNode(x,y,z));
             }
         }
@@ -364,18 +461,15 @@ function getAllNodes(png: any, z: number, imageDimension: Dimension, mapDimensio
  * @param mapDimensions The dimensions of each map
  * @returns A new TraversableMap
  */
-function load(png: any, imageDimension: Dimension, mapDimensions: Dimension): TraversableMap {
-    const grids: Grid = {
-        columns: Math.floor(imageDimension.width / mapDimensions.width), 
-        rows: Math.floor(imageDimension.height / mapDimensions.height),
-    };
+function load(image: Image, png: any, imageDimension: Dimension, mapDimensions: Dimension): TraversableMap {
+    const grid: Grid = getGrid(imageDimension, mapDimensions);
     const fixedNodes: FixedNodes = {};
     const traversableMaps: MapArray[] = [];
 
-    for (let z = 0; z < (grids.columns + grids.rows - 2) + 1; z++) {
-        for (let y = 0; y < grids.rows; y++) {
-            for (let x = 0; x < grids.columns; x++) { 
-                const nodes: { fixedNodes: FixedNodes; allNodes: MapArray; } = getAllNodes(png, z, imageDimension, mapDimensions, grids);
+    for (let z = 0; z < (grid.columns + grid.rows - 2) + 1; z++) {
+        for (let y = 0; y < grid.rows; y++) {
+            for (let x = 0; x < grid.columns; x++) { 
+                const nodes: { fixedNodes: FixedNodes; allNodes: MapArray; } = getAllNodes(png, z, imageDimension, mapDimensions, grid);
                 const map: MapArray = { traversable: nodes.allNodes.traversable, obstacles: nodes.allNodes.obstacles };
                 
                 if(nodes.fixedNodes.origin != undefined) {
@@ -394,6 +488,6 @@ function load(png: any, imageDimension: Dimension, mapDimensions: Dimension): Tr
     if(fixedNodes === undefined || fixedNodes.origin === undefined || fixedNodes.end === undefined) {
         throw new InvalidMapException("Can't obtain a start and/or 'end' positions");
     } else {
-        return { fixedNodes, mapDimensions, traversableMaps, };
+        return { image, fixedNodes, mapDimensions, grid, traversableMaps};
     }
 }
